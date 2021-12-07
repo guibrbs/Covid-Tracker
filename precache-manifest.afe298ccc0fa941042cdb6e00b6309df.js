bed5a3581a181e8c6352ef3cbaed72eb3e851267
guibrbs <200037994@aluno.unb.br>
2021-12-07 20:49:07 -0300

    Updates

---
+++ precache-manifest.afe298ccc0fa941042cdb6e00b6309df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8ac5c215efe9037878fceec1b85d4e",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "262422b012266151ed0e",
+    "url": "/Covid-Tracker/static/css/main.cc2730b0.chunk.css"
+  },
+  {
+    "revision": "f6172934400167fe5b9f",
+    "url": "/Covid-Tracker/static/js/2.300314a8.chunk.js"
+  },
+  {
+    "revision": "84d84091b7fd86ecbd8e49d2e9d52a54",
+    "url": "/Covid-Tracker/static/js/2.300314a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262422b012266151ed0e",
+    "url": "/Covid-Tracker/static/js/main.6fda08a8.chunk.js"
+  },
+  {
+    "revision": "b619aef232c750e36b8d",
+    "url": "/Covid-Tracker/static/js/runtime-main.fe252f1e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-Tracker/static/media/image.d7265326.png"
+  }
+]);
